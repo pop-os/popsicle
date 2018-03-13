d6a7821803b6daee46638b838cca7d2a25620099
Michael Aaron Murphy <mmstickman@gmail.com>
2018-03-13 15:21:39 -0400

    Further Improve Visuals

--- gtk/src/ui/content/flashing.rs
+++ gtk/src/ui/content/flashing.rs
@@ -7,13 +7,16 @@ pub struct FlashView {
 
 impl FlashView {
     pub fn new() -> FlashView {
-        let image = Spinner::new();
-        image.start();
+        let image = Image::new_from_icon_name("drive-removable-media-usb", 6);
         image.set_valign(Align::Start);
 
         let topic = Label::new("Flashing Devices");
         topic.set_halign(Align::Start);
         topic.get_style_context().map(|c| c.add_class("h2"));
+
+        let spinner = Spinner::new();
+        spinner.start();
+        spinner.set_valign(Align::Start);
 
         let description = Label::new("Do not unplug devices while they are being flashed.");
         description.set_halign(Align::Start);
@@ -28,11 +31,15 @@ impl FlashView {
             .map(|c| c.add_class("left-panel"));
         left_panel.pack_start(&image, false, false, 0);
 
+        let topic_container = Box::new(Orientation::Horizontal, 0);
+        topic_container.pack_start(&topic, false, false, 0);
+        topic_container.pack_start(&spinner, false, false, 0);
+
         let right_panel = Box::new(Orientation::Vertical, 0);
         right_panel
             .get_style_context()
             .map(|c| c.add_class("right-panel"));
-        right_panel.pack_start(&topic, false, false, 0);
+        right_panel.pack_start(&topic_container, false, false, 0);
         right_panel.pack_start(&description, false, false, 0);
         right_panel.pack_start(&progress_scroller, true, true, 0);
 
