d94d94c3d8694a8ea1e378fbbc0f6a02f28b7292
Michael Aaron Murphy <mmstickman@gmail.com>
2018-03-22 16:14:55 -0400

    Remove unwrap() in GTK frontend w/ new error view

--- gtk/src/ui/content/mod.rs
+++ gtk/src/ui/content/mod.rs
@@ -1,9 +1,11 @@
 mod devices;
+mod error;
 mod flashing;
 mod images;
 mod summary;
 
 pub use self::devices::DevicesView;
+pub use self::error::ErrorView;
 pub use self::flashing::FlashView;
 pub use self::images::ImageView;
 pub use self::summary::SummaryView;
@@ -14,6 +16,7 @@ pub struct Content {
     pub container:    Stack,
     pub image_view:   ImageView,
     pub devices_view: DevicesView,
+    pub error_view:   ErrorView,
     pub flash_view:   FlashView,
     pub summary_view: SummaryView,
 }
@@ -26,11 +29,13 @@ impl Content {
         let devices_view = DevicesView::new();
         let flash_view = FlashView::new();
         let summary_view = SummaryView::new();
+        let error_view = ErrorView::new();
 
         container.add_named(&image_view.container, "image");
         container.add_named(&devices_view.container, "devices");
         container.add_named(&flash_view.container, "flash");
         container.add_named(&summary_view.container, "summary");
+        container.add_named(&error_view.container, "error");
         container.set_visible_child_name("image");
 
         Content {
@@ -39,6 +44,7 @@ impl Content {
             devices_view,
             flash_view,
             summary_view,
+            error_view,
         }
     }
 }
