908b87feafbefd65f5e6cdf270d1ee06615939b5
Michael Aaron Murphy <mmstickman@gmail.com>
2018-07-28 23:18:32 -0600

    Migrate CLI to BusWriter impl

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,19 +1,23 @@
 //! CLI application for flashing multiple drives in parallel.
 
+extern crate bus_writer;
 extern crate clap;
 extern crate libc;
 extern crate pbr;
 extern crate popsicle;
 
+use bus_writer::*;
 use clap::{App, Arg};
-use pbr::{MultiBar, ProgressBar, Units};
-use std::cell::RefCell;
+use pbr::{MultiBar, Units};
+use std::fs::File;
 use std::io::{self, Write};
-use std::sync::Arc;
+use std::thread::{self, JoinHandle};
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::{Arc, Mutex};
 use std::path::Path;
-use std::{process, thread};
+use std::process;
 
-use popsicle::{mnt, DiskError, Image, PopsicleLog};
+use popsicle::mnt;
 
 fn popsicle() -> Result<(), String> {
     let matches = App::new(env!("CARGO_PKG_NAME"))
@@ -56,14 +60,11 @@ fn popsicle() -> Result<(), String> {
         .get_matches();
 
     let image_path = matches.value_of("IMAGE").expect("IMAGE not set");
-    let mut image = match Image::new(&image_path) {
-        Ok(image) => image,
-        Err(err) => {
-            return Err(format!("error with image at '{}': {}", image_path, err));
-        }
-    };
-
-    let image_size = image.get_size();
+    let mut image = File::open(image_path)
+        .map_err(|why| format!("error with image at '{}': {}", image_path, why))?;
+    let image_size = image.metadata()
+        .map(|x| x.len())
+        .map_err(|why| format!("image metadata error at '{}': {}", image_path, why))?;
 
     let mut disk_args = vec![];
     if matches.is_present("all") {
@@ -93,21 +94,6 @@ fn popsicle() -> Result<(), String> {
         matches.is_present("unmount"),
     ).map_err(|why| format!("disk error: {}", why))?;
 
-    let image_data = {
-        let mut pb = ProgressBar::new(image_size);
-        pb.message("Reading image: ");
-        pb.set_units(Units::Bytes);
-        let mut data = Vec::new();
-        image
-            .read(&mut data, |total| {
-                pb.set(total);
-            })
-            .map_err(|err| format!("image error with image at '{}': {}", image_path, err))?;
-
-        pb.finish();
-        Arc::new(data)
-    };
-
     if !matches.is_present("yes") {
         println!(
             "Are you sure you want to flash '{}' to the following drives?",
@@ -135,47 +121,115 @@ fn popsicle() -> Result<(), String> {
 
     let mut mb = MultiBar::new();
 
-    let mut threads = Vec::new();
-    for (disk_path, mut disk) in disks {
-        let mut pb = mb.create_bar(image_size);
-        pb.message(&format!("W {}: ", disk_path));
-        pb.set_units(Units::Bytes);
-        pb.set(0);
+    let mut paths = Vec::with_capacity(disks.len());
+    let mut destinations = Vec::with_capacity(disks.len());
+    let mut pbs = Vec::with_capacity(disks.len());
+    let errored = Arc::new(
+        (0..disks.len())
+            .map(|_| AtomicBool::new(false))
+            .collect::<Vec<_>>()
+    );
 
-        let image_data = image_data.clone();
-        let pb = RefCell::new(pb);
-        threads.push(thread::spawn(move || -> Result<(), DiskError> {
-            popsicle::write_to_disk(
-                |log| match log {
-                    PopsicleLog::Message(msg) => {
-                        pb.borrow_mut().message(msg);
+    for (disk_path, disk) in disks {
+        let mut pb = mb.create_bar(image_size);
+        pb.set_units(Units::Bytes);
+        pb.message(&format!("W {}: ", disk_path));
+        pbs.push(pb);
+        paths.push(disk_path);
+        destinations.push(disk);
+    }
+
+    let pbs = Arc::new(Mutex::new(pbs));
+    let errored_ = errored.clone();
+    let handle: JoinHandle<io::Result<()>> = thread::spawn(move || {
+        let mut bucket = vec![0u8; 16 * 1024 * 1024];
+        BusWriter::new(
+            &mut image,
+            &mut destinations,
+            |event| {
+                let pbs = &mut pbs.lock().unwrap();
+                match event {
+                    BusWriterMessage::Written { id, bytes_written } => {
+                        pbs[id].set(bytes_written);
                     },
-                    PopsicleLog::Progress(progress) => {
-                        pb.borrow_mut().set(progress);
-                    },
-                    PopsicleLog::Finished => {
-                        pb.borrow_mut().finish();
+                    BusWriterMessage::Completed { id } => {
+                        if check {
+                            pbs[id].set(image_size);
+                        } else {
+                            pbs[id].finish();
+                        }
+                    }
+                    BusWriterMessage::Errored { id, why } => {
+                        let pb = &mut pbs[id];
+                        pb.message(&format!("E {}: {}", paths[id], why));
+                        pb.finish();
+                        errored_[id].store(true, Ordering::SeqCst);
+                    }
+                }
+            },
+            // TODO Ctrl + C signal handling
+            || false
+        ).with_bucket(&mut bucket).write()?;
+
+        let mut unerrored_ids = Vec::new();
+        let mut unerrored_destinations = Vec::new();
+
+        for (id, dest) in destinations.iter().enumerate()
+            .filter(|&(id, _)| !errored_[id].load(Ordering::SeqCst))
+        {
+            unerrored_ids.push(id);
+            unerrored_destinations.push(dest);
+        }
+
+        if check {
+            for (pb, path) in pbs.lock().unwrap().iter_mut().zip(paths.iter()) {
+                pb.set(0);
+                pb.message(&format!("V {}: ", path));
+            }
+
+            BusVerifier::new(
+                image,
+                &mut unerrored_destinations,
+                |event| {
+                    let pbs = &mut pbs.lock().unwrap();
+                    match event {
+                        BusVerifierMessage::Read { id, bytes_read } => {
+                            pbs[unerrored_ids[id]].set(bytes_read);
+                        }
+                        BusVerifierMessage::Invalid { id } => {
+                            pbs[unerrored_ids[id]].finish();
+                            errored_[unerrored_ids[id]].store(true, Ordering::SeqCst);
+                        }
+                        BusVerifierMessage::Valid { id } => {
+                            pbs[unerrored_ids[id]].finish();
+                        }
+                        BusVerifierMessage::Errored { id, why } => {
+                            let pb = &mut pbs[unerrored_ids[id]];
+                            pb.message(&format!("E {}: {}", paths[unerrored_ids[id]], why));
+                            pb.finish();
+                            errored_[unerrored_ids[id]].store(true, Ordering::SeqCst);
+                        }
                     }
                 },
-                disk,
-                &disk_path,
-                image_size,
-                &&image_data,
-                check,
-            )
-        }));
-    }
+                || false
+            ).with_bucket(&mut bucket).verify()
+        } else {
+            Ok(())
+        }
+    });
 
     mb.listen();
 
-    for thread in threads {
-        thread
-            .join()
-            .unwrap()
-            .map_err(|why| format!("disk error: {}", why))?;
-    }
-
-    Ok(())
+    handle.join()
+        .unwrap()
+        .map_err(|why| format!("bus failed: {}", why))
+        .and_then(|_| {
+            if errored.iter().any(|x| x.load(Ordering::SeqCst)) {
+                Err("a device failed".to_owned())
+            } else {
+                Ok(())
+            }
+        })
 }
 
 fn main() {
