e9ae7bd1963ea5fc56b07b6436e2ce887e3f8cb5
stratact <stratact1@gmail.com>
2019-04-02 12:08:15 -0700

    Revert change

--- gtk/src/app/signals/images.rs
+++ gtk/src/app/signals/images.rs
@@ -34,7 +34,7 @@ impl App {
         misc::drag_and_drop(&image_view, move |data| {
             if let Some(uri) = data.get_text() {
                 if uri.starts_with("file://") {
-                    let path = Path::new(&uri[7..]);
+                    let path = Path::new(&uri[7..uri.len() - 1]);
                     if path.extension().map_or(false, |ext| ext == "iso" || ext == "img")
                         && path.exists()
                     {
