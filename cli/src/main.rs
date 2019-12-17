a078f50debc9db3b11f09e63e0a2047e3efdb006
Michael Aaron Murphy <mmstick@pm.me>
2019-12-17 15:54:57 -0700

    feat(cli): Use machine-readable output for cli when stdout is not a tty

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -5,15 +5,22 @@ extern crate anyhow;
 #[macro_use]
 extern crate cascade;
 #[macro_use]
+extern crate derive_new;
+#[macro_use]
 extern crate fomat_macros;
 
 use anyhow::Context;
 use async_std::{
-    fs::File,
+    fs::OpenOptions,
+    os::unix::fs::OpenOptionsExt,
     path::{Path, PathBuf},
 };
 use clap::{App, Arg, ArgMatches};
-use futures::{channel::oneshot, executor};
+use futures::{
+    channel::{mpsc, oneshot},
+    executor, join,
+    prelude::*,
+};
 use pbr::{MultiBar, Pipe, ProgressBar, Units};
 use popsicle::{mnt, Progress, Task};
 use std::{
@@ -68,7 +75,10 @@ async fn popsicle(
 ) -> anyhow::Result<()> {
     let image_path = matches.value_of("IMAGE").expect("IMAGE not set");
 
-    let image = File::open(image_path)
+    let image = OpenOptions::new()
+        .custom_flags(libc::O_SYNC)
+        .read(true)
+        .open(image_path)
         .await
         .with_context(|| format!("error with image at '{}'", image_path))?;
 
@@ -96,7 +106,9 @@ async fn popsicle(
             .await
             .context("failed to open disks")?;
 
-    if !matches.is_present("yes") {
+    let is_tty = atty::is(atty::Stream::Stdout);
+
+    if is_tty && !matches.is_present("yes") {
         epint!(
             "Are you sure you want to flash '" (image_path) "' to the following drives?\n"
             for (path, _) in &disks {
@@ -117,45 +129,134 @@ async fn popsicle(
 
     let check = matches.is_present("check");
 
-    println!();
+    // If this is a TTY, display a progress bar. If not, display machine-readable info.
+    if is_tty {
+        println!();
 
-    let mut mb = MultiBar::new();
-    let mut task = Task::new(image, check);
+        let mut mb = MultiBar::new();
+        let mut task = Task::new(image, check);
 
-    for (disk_path, disk) in disks {
-        let pb = CliProgress(cascade! {
-            mb.create_bar(image_size);
-            ..set_units(Units::Bytes);
-            ..message(&format!("W {}: ", disk_path.display()));
+        for (disk_path, disk) in disks {
+            let pb = InteractiveProgress::new(cascade! {
+                mb.create_bar(image_size);
+                ..set_units(Units::Bytes);
+                ..message(&format!("W {}: ", disk_path.display()));
+            });
+
+            task.subscribe(disk, disk_path, pb);
+        }
+
+        thread::spawn(|| {
+            executor::block_on(async move {
+                let buf = &mut [0u8; 64 * 1024];
+                let _ = rtx.send(task.process(buf).await);
+            })
         });
 
-        task.subscribe(disk, disk_path, pb);
-    }
+        mb.listen();
+    } else {
+        let (etx, erx) = mpsc::unbounded();
+        let mut paths = Vec::new();
+        let mut task = Task::new(image, check);
 
-    thread::spawn(move || {
-        executor::block_on(async move {
+        for (disk_path, disk) in disks {
+            let pb = MachineProgress::new(paths.len(), etx.clone());
+            paths.push(disk_path.clone());
+            task.subscribe(disk, disk_path, pb);
+        }
+
+        drop(etx);
+
+        let task = async move {
             let buf = &mut [0u8; 64 * 1024];
             let _ = rtx.send(task.process(buf).await);
-        })
-    });
+        };
 
-    mb.listen();
+        join!(machine_output(erx, &paths, image_size), task);
+    }
 
     Ok(())
 }
 
-pub struct CliProgress(ProgressBar<Pipe>);
+/// An event for creating a machine-readable output
+pub enum Event {
+    Message(usize, Box<str>),
+    Finished(usize),
+    Set(usize, u64),
+}
 
-impl Progress for CliProgress {
-    fn message(&mut self, message: &str) {
-        self.0.message(message);
+/// Tracks progress
+#[derive(new)]
+pub struct MachineProgress {
+    id: usize,
+
+    handle: mpsc::UnboundedSender<Event>,
+}
+
+impl Progress for MachineProgress {
+    fn message(&mut self, _path: &Path, kind: &str, message: &str) {
+        let _ = self.handle.unbounded_send(Event::Message(
+            self.id,
+            if message.is_empty() { kind.into() } else { [kind, " ", message].concat().into() },
+        ));
     }
 
     fn finish(&mut self) {
-        self.0.finish();
+        let _ = self.handle.unbounded_send(Event::Finished(self.id));
     }
 
     fn set(&mut self, written: u64) {
-        self.0.set(written);
+        let _ = self.handle.unbounded_send(Event::Set(self.id, written));
+    }
+}
+
+#[derive(new)]
+pub struct InteractiveProgress {
+    pipe: ProgressBar<Pipe>,
+}
+
+impl Progress for InteractiveProgress {
+    fn message(&mut self, path: &Path, kind: &str, message: &str) {
+        self.pipe.message(&format!("{} {}: {}", kind, path.display(), message));
+    }
+
+    fn finish(&mut self) {
+        self.pipe.finish();
+    }
+
+    fn set(&mut self, written: u64) {
+        self.pipe.set(written);
+    }
+}
+
+/// Writes a machine-friendly output, when this program is being piped into another.
+async fn machine_output(
+    mut rx: mpsc::UnboundedReceiver<Event>,
+    paths: &[Box<Path>],
+    image_size: u64,
+) {
+    let stdout = io::stdout();
+    let stdout = &mut stdout.lock();
+
+    let _ = wite!(
+        stdout,
+        "Size(" (image_size) ")\n"
+        for path in paths {
+            "Device(\"" (path.display()) "\")\n"
+        }
+    );
+
+    while let Some(event) = rx.next().await {
+        match event {
+            Event::Message(id, message) => {
+                let _ = witeln!(stdout, "Message(\"" (paths[id].display()) "\",\"" (message) "\")");
+            }
+            Event::Finished(id) => {
+                let _ = witeln!(stdout, "Finished(\"" (paths[id].display()) "\")");
+            }
+            Event::Set(id, written) => {
+                let _ = witeln!(stdout, "Set(\"" (paths[id].display()) "\"," (written) ")");
+            }
+        }
     }
 }
