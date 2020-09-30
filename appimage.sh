16f35207eff6910769da0a38cdf2ed1bde550def
Ian Douglas Scott <idscott@system76.com>
2020-09-30 13:52:07 -0700

    ci: Build appimage

---
+++ appimage.sh
@@ -0,0 +1,8 @@
+set -e
+
+export APPIMAGE_EXTRACT_AND_RUN=1
+apt-get update
+apt-get install -y libgtk-3-dev
+wget https://github.com/TheAssassin/appimagecraft/releases/download/continuous/appimagecraft-x86_64.AppImage
+chmod +x appimagecraft-x86_64.AppImage
+./appimagecraft-x86_64.AppImage
