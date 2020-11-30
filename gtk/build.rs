1685d8004b6d11a23f0fd0e1440cf0cc9c8e8d2a
Ian Douglas Scott <idscott@system76.com>
2020-11-30 14:18:52 -0800

    fix: Embed icons in gresource to work with non-Pop icon theme (#115)
    https://github.com/pop-os/popsicle/issues/110#issuecomment-732417652
    notes that three of the icons Popsicle uses are non-standard. The
    standard icon does not need to be included.
    
    Fixes https://github.com/pop-os/popsicle/issues/110

---
+++ gtk/build.rs
@@ -0,0 +1,25 @@
+use std::{
+    env,
+    fs,
+    process::{self, Command},
+};
+
+fn main() {
+    for i in fs::read_dir("assets").unwrap() {
+        println!("cargo:rerun-if-changed={}", i.unwrap().path().display());
+    }
+
+    let out_dir = env::var("OUT_DIR").unwrap();
+
+    let status = Command::new("glib-compile-resources")
+        .arg("--sourcedir=assets")
+        .arg(&format!("--target={}/compiled.gresource", out_dir))
+        .arg("assets/resources.gresource.xml")
+        .status()
+        .unwrap();
+
+    if !status.success() {
+        eprintln!("glib-compile-resources failed with exit status {}", status);
+        process::exit(1);
+    }
+}
