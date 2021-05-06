119beaacd603d89ff28b965f90dfa456f059c5c6
Ian Douglas Scott <idscott@system76.com>
2021-05-06 10:34:33 -0700

    fix(ci): Missing dependency for AppImage build

--- appimage.sh
+++ appimage.sh
@@ -2,7 +2,7 @@ set -e
 
 export APPIMAGE_EXTRACT_AND_RUN=1
 apt-get update
-apt-get install -y libgtk-3-dev
+apt-get install -y libgtk-3-dev patchelf
 wget https://github.com/TheAssassin/appimagecraft/releases/download/continuous/appimagecraft-x86_64.AppImage
 chmod +x appimagecraft-x86_64.AppImage
 ./appimagecraft-x86_64.AppImage
