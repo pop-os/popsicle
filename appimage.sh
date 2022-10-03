6984621b31d0c0c7670e4b909f0007e272f280c1
Michael Aaron Murphy <mmstick@pm.me>
2022-10-03 16:48:34 +0200

    fix(appimage): Missing dependencies

--- appimage.sh
+++ appimage.sh
@@ -2,7 +2,7 @@ set -e
 
 export APPIMAGE_EXTRACT_AND_RUN=1
 apt-get update
-apt-get install -y libgtk-3-dev patchelf
+apt-get install -y help2man libclang-dev libgtk-3-dev patchelf
 wget https://github.com/TheAssassin/appimagecraft/releases/download/continuous/appimagecraft-x86_64.AppImage
 chmod +x appimagecraft-x86_64.AppImage
 ./appimagecraft-x86_64.AppImage
