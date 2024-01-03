689f40825d67a05027010cf20978b89aa9ea2da3
Michael Aaron Murphy <michael@mmurphy.dev>
2024-01-03 16:38:44 +0100

    chore(github): fix dubious ownership error in docker container

--- appimage.sh
+++ appimage.sh
@@ -1,6 +1,7 @@
 set -e
 
 export APPIMAGE_EXTRACT_AND_RUN=1
+git config --global --add safe.directory /github/workspace
 apt-get update
 apt-get install -y help2man libclang-dev libgtk-3-dev patchelf
 wget https://github.com/TheAssassin/appimagecraft/releases/download/continuous/appimagecraft-x86_64.AppImage
